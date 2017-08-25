4aad4ad71eab9f7803b55dc0691bce4ac37062e7
E Jun <jason_e@syscom.com.cn>
2017-08-25 14:33:55 +0800

    add a basic RoleCardView

---
+++ app/src/main/java/com/example/phoenixtree/util/RoleCardView.java
@@ -0,0 +1,59 @@
+package com.example.phoenixtree.util;
+
+import android.content.Context;
+import android.support.v7.widget.CardView;
+import android.util.AttributeSet;
+
+/**
+ * Created by ej on 8/25/2017.
+ */
+
+public class RoleCardView extends CardView {
+
+    // private float[] vertices = new float[3];
+    private int realHeight;
+    private int realWidth;
+
+    public RoleCardView(Context context) {
+        super(context);
+    }
+
+    public RoleCardView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public RoleCardView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+/*
+    public float[] getVertices() {
+        return vertices;
+    }
+
+    public void setVertices(float[] vertices) {
+        this.vertices = vertices;
+    }
+*/
+    public int getRealHeight() {
+        return realHeight;
+    }
+
+    public void setRealHeight(int realHeight) {
+        this.realHeight = realHeight;
+    }
+
+    public int getRealWidth() {
+        return realWidth;
+    }
+
+    public void setRealWidth(int realWidth) {
+        this.realWidth = realWidth;
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(
+                MeasureSpec.makeMeasureSpec(realWidth, MeasureSpec.EXACTLY),
+                MeasureSpec.makeMeasureSpec(realHeight, MeasureSpec.EXACTLY));
+    }
+}
