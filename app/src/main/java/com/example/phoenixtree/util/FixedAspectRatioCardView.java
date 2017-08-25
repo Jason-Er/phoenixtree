ee27d090a0b47ebca961292366619dc4acbd0592
E Jun <jason_e@syscom.com.cn>
2017-08-25 11:39:35 +0800

    add a new view class for stage view which is fixed aspect ratio

---
+++ app/src/main/java/com/example/phoenixtree/util/FixedAspectRatioCardView.java
@@ -0,0 +1,69 @@
+package com.example.phoenixtree.util;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.support.v7.widget.CardView;
+import android.util.AttributeSet;
+
+import com.example.phoenixtree.R;
+
+/**
+ * Created by ej on 8/25/2017.
+ */
+
+public class FixedAspectRatioCardView extends CardView {
+
+    private float ratio;
+
+    public FixedAspectRatioCardView(Context context) {
+        super(context);
+    }
+
+    public FixedAspectRatioCardView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init(context, attrs);
+    }
+
+    public FixedAspectRatioCardView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init(context, attrs);
+    }
+
+    private void init(Context context, AttributeSet attrs) {
+        TypedArray a = context.getTheme().obtainStyledAttributes(
+                attrs,
+                R.styleable.FixedAspectRatioCardView,
+                0, 0);
+        try {
+            ratio = a.getFloat(R.styleable.FixedAspectRatioCardView_ratio, 0);
+        } finally {
+            a.recycle();
+        }
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        int originalWidth = MeasureSpec.getSize(widthMeasureSpec);
+
+        int originalHeight = MeasureSpec.getSize(heightMeasureSpec);
+
+        int calculatedHeight = (int)(originalWidth / ratio);
+
+        int finalWidth, finalHeight;
+
+        if (calculatedHeight > originalHeight)
+        {
+            finalWidth = (int)(originalHeight * ratio);
+            finalHeight = originalHeight;
+        }
+        else
+        {
+            finalWidth = originalWidth;
+            finalHeight = calculatedHeight;
+        }
+
+        super.onMeasure(
+                MeasureSpec.makeMeasureSpec(finalWidth, MeasureSpec.EXACTLY),
+                MeasureSpec.makeMeasureSpec(finalHeight, MeasureSpec.EXACTLY));
+    }
+}
