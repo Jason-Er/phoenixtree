6a883a65708fef03922f99a8f2d46920b87543df
E Jun <jason_e@syscom.com.cn>
2017-09-06 10:51:00 +0800

    add AppExecutor

--- app/src/main/java/com/example/phoenixtree/repository/SceneRepository.java
+++ app/src/main/java/com/example/phoenixtree/repository/SceneRepository.java
@@ -5,6 +5,7 @@ import android.arch.lifecycle.MutableLiveData;
 
 import com.example.phoenixtree.Model.Resource;
 import com.example.phoenixtree.Model.Scene;
+import com.example.phoenixtree.app.AppExecutors;
 import com.example.phoenixtree.dataservice.remote.WebService;
 import com.example.phoenixtree.util.Fake;
 
@@ -17,17 +18,14 @@ import javax.inject.Singleton;
 @Singleton
 public class SceneRepository {
     private final WebService webservice;
-    // private final Executor executor;
+    private final AppExecutors appExecutors;
 
     final MutableLiveData<Resource<Scene>> liveData = new MutableLiveData<>();
 
     @Inject
-    public SceneRepository(WebService webservice /*, Executor executor*/) {
-        
+    public SceneRepository(WebService webservice, AppExecutors appExecutors) {
         this.webservice = webservice;
-        /*
-        this.executor = executor;
-        */
+        this.appExecutors = appExecutors;
     }
 
     public LiveData<Resource<Scene>> getScene(long sceneId) {
