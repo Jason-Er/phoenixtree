2fdea8582c839e12a30c85314ccf83a239851857
E Jun <jason_e@syscom.com.cn>
2017-12-22 16:36:51 +0800

    adjust imageButton to its right place

--- app/src/main/java/com/example/phoenixtree/view/topBar/CustomTopBar.java
+++ app/src/main/java/com/example/phoenixtree/view/topBar/CustomTopBar.java
@@ -4,13 +4,13 @@ import android.content.Context;
 import android.support.annotation.Nullable;
 import android.support.design.widget.CoordinatorLayout;
 import android.util.AttributeSet;
-import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 
 /**
  * Created by ej on 12/22/2017.
  */
 @CoordinatorLayout.DefaultBehavior(CustomTopBarBehavior.class)
-public class CustomTopBar extends LinearLayout {
+public class CustomTopBar extends RelativeLayout {
     public CustomTopBar(Context context) {
         super(context);
     }
